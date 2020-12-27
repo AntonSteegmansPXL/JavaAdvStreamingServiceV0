c4a47318608adf7e8b27b5902f757382983db0ca
Anton Steegmans <jouwEmailadres>
2020-12-27 15:00:23 +0100

    package repository toegevoegd

--- src/main/java/be/pxl/ja/streamingservice/model/StreamingPlan.java
+++ src/main/java/be/pxl/ja/streamingservice/model/StreamingPlan.java
@@ -1,15 +1,21 @@
 package be.pxl.ja.streamingservice.model;
 
+import java.math.BigDecimal;
+
 public enum StreamingPlan {
-    BASIC(1, 7.99),
-    STANDAARD(2, 11.99),
-    PREMIUM(4, 15.99);
+    BASIC(1, BigDecimal.valueOf(7.99)),
+    STANDARD(2, BigDecimal.valueOf(11.99)),
+    PREMIUM(4, BigDecimal.valueOf(15.99));
 
-    private int numberOfProfiles;
-    private double price;
+    private int numberOfScreens;
+    private BigDecimal price;
 
-    StreamingPlan(int numberOfProfiles, double price) {
-        this.numberOfProfiles = numberOfProfiles;
+    StreamingPlan(int numberOfScreens, BigDecimal price) {
+        this.numberOfScreens = numberOfScreens;
         this.price = price;
+    }
+
+    public int getNumberOfScreens() {
+        return numberOfScreens;
     }
 }
--- src/main/java/be/pxl/ja/streamingservice/model/TVShow.java
+++ src/main/java/be/pxl/ja/streamingservice/model/TVShow.java
@@ -3,6 +3,7 @@ package be.pxl.ja.streamingservice.model;
 public final class TVShow extends Content{
 
     private int numberOfSeasons;
+
     public TVShow(String title, Rating maturityRating, int numberOfSeasons) {
         super(title, maturityRating);
         this.numberOfSeasons = numberOfSeasons;
