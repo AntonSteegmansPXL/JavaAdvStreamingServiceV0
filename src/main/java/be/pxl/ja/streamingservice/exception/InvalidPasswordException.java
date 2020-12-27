f5c7b8825db08d6a31bf5aaee53176178b59650e
Anton Steegmans <jouwEmailadres>
2020-12-27 15:02:33 +0100

    exceptions toegevoegd

---
+++ src/main/java/be/pxl/ja/streamingservice/exception/InvalidPasswordException.java
@@ -0,0 +1,9 @@
+package be.pxl.ja.streamingservice.exception;
+
+public class InvalidPasswordException extends Exception {
+
+    public InvalidPasswordException() {
+        super("Password not correct.");
+    }
+
+}
